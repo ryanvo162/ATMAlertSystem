70ff98806cc23dc1b5157bcb89042ae3aabc5ee4
ryanvo162 <ryanvo.0162@gmail.com>
2021-12-04 20:49:12 +0700

    Cập nhật màn hình
    Màn hình chào, màn hình đăng nhập

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Splash from './screens/SplashScreen';
+import HomeScreen from './screens/HomeScreen';
+import { useFonts } from 'expo-font';
+
+
+// import InformationScreen from './components/InformationScreen';
+// import DoneScreen from './components/DoneScreen';
+// import ErrorScreen from './components/ErrorScreen';
+// import OutOfNoodlesScreen from './components/OutOfNoodlesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Mulish: require('./assets/fonts/Mulish-Regular.ttf'),
+    MulishExtraBold: require('./assets/fonts/Mulish-ExtraBold.ttf'),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome" >
+        <Stack.Screen name="Welcome" component={Splash} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        {/* <Stack.Screen name="Done" component={DoneScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="SoldOut" component={OutOfNoodlesScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Error" component={ErrorScreen} options={{ headerShown: false }} /> */}
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/SplashScreen.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+
+export default function Splash({ navigation }: { navigation: any }) {
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await new Promise(() => setTimeout(() => { navigation.replace('Home') }, 2000));
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Image source={require('../assets/images/logo.png')} />
+            <Text style={styles.version}>Version 1.0.0.2</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:'white'
+    },
+    version: {
+        position: 'absolute',
+        fontSize: 14,
+        bottom: 20,
+        fontFamily: 'Mulish',
+        color: '#848f9d'
+    }
+});
